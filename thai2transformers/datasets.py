fe41e3a57a484743899d37d736bd3663ddb5297b
cstorm125 <admin@gpu-k80-x1.asia-east1-b.c.vistec-depa-ai.internal>
2020-07-17 16:32:16 +0000

    mlm trainer

--- thai2transformers/datasets.py
+++ thai2transformers/datasets.py
@@ -6,6 +6,48 @@ import torch
 from torch.utils.data import Dataset
 from transformers.data.processors.utils import InputFeatures
 
+class MLMDataset(Dataset):
+    def __init__(
+        self,
+        tokenizer,
+        data_dir,
+        max_length=512,
+        bs = 10000,
+    ):
+        self.fnames = glob.glob(f"{data_dir}/*.txt")
+        self.max_length = max_length
+        self.tokenizer = tokenizer
+        self.bs = bs
+        self.features = []
+        self._build()
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, i):
+        return self.features[i]
+    
+    def _build(self):
+        input_ids = []
+        for fname in tqdm(self.fnames):
+            with open(fname,'r') as f:
+                df = f.readlines()
+                for i in tqdm(range(math.ceil(len(df)/self.bs))):
+                    texts = list(df[i*self.bs:(i+1)*self.bs])
+                    #tokenize
+                    tokenized_inputs = self.tokenizer(
+                        texts,
+                        max_length=self.max_length,
+                        truncation=True,
+                        pad_to_max_length=False,
+                    )
+                    #add to list
+                    input_ids+=tokenized_inputs['input_ids']
+
+        for i in tqdm(range(len(input_ids))):
+            feature = torch.tensor(input_ids[i], dtype=torch.long)
+            self.features.append(feature)
+
 class SequenceClassificationDataset(Dataset):
     def __init__(
         self,
