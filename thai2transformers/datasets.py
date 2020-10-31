eae0f3b3298567d5d3fe4b61ff405b974064a7a2
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2020-10-31 08:32:53 +0700

    Update frequency

--- thai2transformers/datasets.py
+++ thai2transformers/datasets.py
@@ -101,8 +101,10 @@ class MLMDataset(Dataset):
     def convert(self):
         for i in tqdm(range(len(self.features))):
             self.features[i] = torch.tensor(self.features[i], dtype=torch.long)
-            if i % 25_000_000 == 0 and i != 0:
+            if i % 100_000_000 == 0 and i != 0:
+                print('[INFO] Apply gc.collect()')
                 gc.collect()
+                print('[INFO] Done applying gc.collect()')
 
     def dump_chunk(self, start, stop):
         dirname = os.path.dirname(self.binarized_path)
