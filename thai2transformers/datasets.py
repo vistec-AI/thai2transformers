90e42ed9ccb6e492bff024b58b9849eed536091d
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2020-11-01 15:32:08 +0700

    Write bin file

--- thai2transformers/datasets.py
+++ thai2transformers/datasets.py
@@ -103,7 +103,7 @@ class MLMDataset(Dataset):
         self.features = [ i for lst in results for i in lst ]
 
         print('[INFO] Done.')
-        # self.write_binarized_features(None)
+        self.write_binarized_features(None)
 
     def convert(self):
         for i in tqdm(range(len(self.features))):
