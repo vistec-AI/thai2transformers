86d3066dadb1bd2f173b66f45ef9b4282e935d31
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2020-11-01 12:17:37 +0700

    Change bz

--- thai2transformers/datasets.py
+++ thai2transformers/datasets.py
@@ -13,7 +13,7 @@ nb_cores = multiprocessing.cpu_count()
     
 class MLMDataset(Dataset):
     def __init__(
-        self, tokenizer, data_dir, max_length=512, binarized_path=None, ext=".txt", bs=10000,
+        self, tokenizer, data_dir, max_length=512, binarized_path=None, ext=".txt", bs=5000,
         parallelize=True, chunksize=1000
     ):
         self.fnames = glob.glob(f"{data_dir}/*{ext}")
