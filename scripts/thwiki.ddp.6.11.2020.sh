cfebdc81991e8c7d5ef73ab017129c722da7964a
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2020-11-07 10:24:51 +0700

    Updat script

--- scripts/thwiki.ddp.6.11.2020.sh
+++ scripts/thwiki.ddp.6.11.2020.sh
@@ -1,41 +1,46 @@
-export N_NODES=$1
-export NODE_RANK=${SLURM_PROCID}
-export HOSTNAME=$2
-export N_GPU_NODE=$3
-
-echo "Rank $NODE_RANK"
+#!/bin/bash
+N_NODES=$1
+NODE_RANK=${SLURM_PROCID}
+HOSTNAME=$2
+N_GPU_NODE=$3
+JOBID=$4
+echo "Node rank $NODE_RANK" |& tee -a ./slurm_logs/thwiki.ddp.6.11.2020.rank-$NODE_RANK.out
 
 export MASTER_PORT=9999
 export MASTER_ADDR=$HOSTNAME
 
-python -m torch.distributed.launch \
+echo "Total steps = 500,000 / 8 (15,625 steps per GPU)"
+echo "--learning_rate 6e-4 "
+
+EXP_NAME=exp012_thwiki-for-ddp_6.11.2020_spm_vs-24k_fp16_bz32_maxstep-500k_ngpus-32_maxseqlen-512_mlmdataset
+
+WANDBpython -m torch.distributed.launch \
     --nproc_per_node=$N_GPU_NODE \
     --nnodes=$N_NODES \
     --node_rank $NODE_RANK \
     --master_addr $MASTER_ADDR \
     --master_port $MASTER_PORT \
-    python train_mlm_camembert_thai_ddp.py \
+    train_mlm_camembert_thai.ddp.py \
     --tokenizer_name_or_path ../dataset/spm/thwiki-for-ddp_6.11.2020_spm_vs-24k/sentencepiece.bpe.model \
     --ext txt \
     --train_dir ../dataset/split/thwiki-for-ddp_6.11.2020/train/ \
     --eval_dir ../dataset/split/thwiki-for-ddp_6.11.2020/val/ \
     --block_size 512 \
-    --learning_rate 3e-4 --weight_decay 0.01 \
+    --learning_rate 6e-4 --weight_decay 0.01 \
     --adam_epsilon 1e-6 \
     --fp16 True \
     --max_steps 15625 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 16 \
     --gradient_accumulation_steps 16 \
-    --warmup_steps 500 \
+    --warmup_steps 24000 \
     --seed 2020 \
-    --save_steps 2500 \
-    --logging_steps 1 \
-    --save_total_limit 50 \
-    --evaluate_during_training \
-    --eval_steps 1000 \
-    --logging_dir ../logs/exp012_thwiki-for-ddp_6.11.2020_spm_vs-24k_fp16_bz32_maxstep-500k_ngpus-32_maxseqlen-512_mlmdataset \
-    --output_dir ../checkpoints/exp011_thwiki-for-ddp_6.11.2020_spm_vs-24k_fp16_bz32_maxstep-500k_ngpus-32_maxseqlen-512_mlmdataset \
+    --save_steps 5000 \
+    --logging_steps 5 \
+    --save_total_limit 100 \
+    --evaluation_strategy steps \
+    --eval_steps 2500 \
+    --logging_dir /ist/ist-share/scads/aires/thai2transformers_store/logs/exp012_thwiki-for-ddp_6.11.2020_spm_vs-24k_fp16_bz32_maxstep-500k_ngpus-32_maxseqlen-512_mlmdataset \
+    --output_dir /ist/ist-share/scads/aires/thai2transformers_store/checkpoints/exp011_thwiki-for-ddp_6.11.2020_spm_vs-24k_fp16_bz32_maxstep-500k_ngpus-32_maxseqlen-512_mlmdataset \
     --add_space_token \
-    --datasets_cache_dir ../dataset/binarized/thwiki-for-ddp_4.11.2020 \
-    --preprocessing_num_workers 20
+    --datasets_cache_dir ../dataset/binarized/thwiki-for-ddp_4.11.2020 |& tee -a ./slurm_logs/thwiki.ddp.6.11.2020.j-$JOBID.rank-$NODE_RANK.out
