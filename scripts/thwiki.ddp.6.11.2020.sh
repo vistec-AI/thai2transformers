e03fd4c0a6d72df1b6b2e325904a6ff01a6728f1
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2020-11-13 21:01:28 +0700

    Update script

--- scripts/thwiki.ddp.6.11.2020.sh
+++ scripts/thwiki.ddp.6.11.2020.sh
@@ -25,17 +25,11 @@ echo "Node rank $NODE_RANK" |& tee -a ./slurm_logs/thwiki.ddp.6.11.2020.rank-$NO
 export MASTER_PORT=9999
 export MASTER_ADDR=$HOSTNAME
 
-echo "Total steps = 500,000 / 32 (15,625 steps per GPU)"
+echo "Total steps = $MAX_STEPS"
 echo "--learning_rate $LR "
 
 module load CUDA/10.2
 
-# EXP_NAME=exp012_thwiki-for-ddp_6.11.2020_spm_vs-24k_fp16_bz32_maxstep-500k_ngpus-32_maxseqlen-512_mlmdataset
-if [[ "$MODEL_CHECKPOINT_DIR" != "" ]]; then
-  echo "Resume model training from $MODEL_CHECKPOINT_DIR"
-else 
-  MODEL_CHECKPOINT_DIR=''
-fi
 
 if [[ "$NODE_RANK" != "0" ]]; then
   delay=`expr 5 + $NODE_RANK `     # Whitespace for expr is important
@@ -49,14 +43,15 @@ echo "Global warmup_steps  = $WARMUP_STEPS  " |& tee -a ./slurm_logs/thwiki.ddp.
 echo "Global save_steps    = $SAVE_STEPS    " |& tee -a ./slurm_logs/thwiki.ddp.6.11.2020.rank-$NODE_RANK.out
 echo "Global eval_steps    = $EVAL_STEPS    " |& tee -a ./slurm_logs/thwiki.ddp.6.11.2020.rank-$NODE_RANK.out
 
-
 export WANDB_WATCH=true
 export WANDB_MODE=dryrun
 export WANDB_PROJECT=thai2transformers
 export WANDB_ENTITY=lalital
 export WANDB_DIR=/ist/ist-share/scads/aires/thai2transformers_store/wandb_logs/ \
 export WANDB_NAME=$EXP_NAME
-python -m torch.distributed.launch \
+if [[ "$MODEL_CHECKPOINT_DIR" != "" ]]; then
+  echo "Resume model training from $MODEL_CHECKPOINT_DIR"
+  python -m torch.distributed.launch \
 		--nproc_per_node=$N_PROC_PER_NODE \
     --nnodes=$N_NODES \
     --node_rank $NODE_RANK \
@@ -88,3 +83,38 @@ python -m torch.distributed.launch \
     --datasets_cache_dir ../dataset/binarized/thwiki-for-ddp_6.11.2020/ \
     --model_directory $MODEL_CHECKPOINT_DIR \
     --dataset_loader_name linebyline |& tee -a ./slurm_logs/$EXP_NAME.job-$JOBID.rank-$NODE_RANK.out
+
+else 
+  
+  python -m torch.distributed.launch \
+      --nproc_per_node=$N_PROC_PER_NODE \
+      --nnodes=$N_NODES \
+      --node_rank $NODE_RANK \
+      --master_addr $MASTER_ADDR \
+      --master_port $MASTER_PORT \
+    ./train_mlm_camembert_thai.ddp.py \
+      --tokenizer_name_or_path ../dataset/spm/thwiki-for-ddp_6.11.2020_spm_vs-24k/sentencepiece.bpe.model \
+      --ext txt \
+      --train_path ../dataset/split/thwiki-for-ddp_6.11.2020/train/train.txt \
+      --eval_path ../dataset/split/thwiki-for-ddp_6.11.2020/val/val.txt \
+      --block_size 510 \
+      --learning_rate $LR --weight_decay 0.01 \
+      --adam_epsilon 1e-6 \
+      --fp16 True \
+      --max_steps $MAX_STEPS \
+      --per_device_train_batch_size $BATCH_SIZE \
+      --per_device_eval_batch_size $BATCH_SIZE \
+      --gradient_accumulation_steps $GRAD_ACC \
+      --warmup_steps $WARMUP_STEPS \
+      --seed 2020 \
+      --save_steps $SAVE_STEPS \
+      --logging_steps 10 \
+      --save_total_limit 100 \
+      --evaluation_strategy steps \
+      --eval_steps $EVAL_STEPS \
+      --logging_dir /ist/ist-share/scads/aires/thai2transformers_store/logs/$EXP_NAME/ \
+      --output_dir /ist/ist-share/scads/aires/thai2transformers_store/checkpoints/$EXP_NAME/ \
+      --add_space_token \
+      --datasets_cache_dir ../dataset/binarized/thwiki-for-ddp_6.11.2020/ \
+      --dataset_loader_name linebyline |& tee -a ./slurm_logs/$EXP_NAME.job-$JOBID.rank-$NODE_RANK.out
+fi
