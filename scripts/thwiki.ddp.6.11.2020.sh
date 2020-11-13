7a79d42b2f48105b84ae7691c23fd85152653d7a
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2020-11-13 21:18:57 +0700

    Update condition

--- scripts/thwiki.ddp.6.11.2020.sh
+++ scripts/thwiki.ddp.6.11.2020.sh
@@ -49,71 +49,36 @@ export WANDB_PROJECT=thai2transformers
 export WANDB_ENTITY=lalital
 export WANDB_DIR=/ist/ist-share/scads/aires/thai2transformers_store/wandb_logs/ \
 export WANDB_NAME=$EXP_NAME
-if [[ "$MODEL_CHECKPOINT_DIR" == "none" ]]; then
-  echo "Resume model training from $MODEL_CHECKPOINT_DIR"
-  python -m torch.distributed.launch \
-		--nproc_per_node=$N_PROC_PER_NODE \
-    --nnodes=$N_NODES \
-    --node_rank $NODE_RANK \
-    --master_addr $MASTER_ADDR \
-    --master_port $MASTER_PORT \
-   ./train_mlm_camembert_thai.ddp.py \
-    --tokenizer_name_or_path ../dataset/spm/thwiki-for-ddp_6.11.2020_spm_vs-24k/sentencepiece.bpe.model \
-    --ext txt \
-    --train_path ../dataset/split/thwiki-for-ddp_6.11.2020/train/train.txt \
-    --eval_path ../dataset/split/thwiki-for-ddp_6.11.2020/val/val.txt \
-    --block_size 510 \
-    --learning_rate $LR --weight_decay 0.01 \
-    --adam_epsilon 1e-6 \
-    --fp16 True \
-    --max_steps $MAX_STEPS \
-    --per_device_train_batch_size $BATCH_SIZE \
-    --per_device_eval_batch_size $BATCH_SIZE \
-    --gradient_accumulation_steps $GRAD_ACC \
-    --warmup_steps $WARMUP_STEPS \
-    --seed 2020 \
-    --save_steps $SAVE_STEPS \
-    --logging_steps 10 \
-    --save_total_limit 100 \
-    --evaluation_strategy steps \
-    --eval_steps $EVAL_STEPS \
-    --logging_dir /ist/ist-share/scads/aires/thai2transformers_store/logs/$EXP_NAME/ \
-    --output_dir /ist/ist-share/scads/aires/thai2transformers_store/checkpoints/$EXP_NAME/ \
-    --add_space_token \
-    --datasets_cache_dir ../dataset/binarized/thwiki-for-ddp_6.11.2020/ \
-    --dataset_loader_name linebyline |& tee -a ./slurm_logs/$EXP_NAME.job-$JOBID.rank-$NODE_RANK.out
-fi
 
-if [[ "$MODEL_CHECKPOINT_DIR" != "none" ]]; then
-  python -m torch.distributed.launch \
-      --nproc_per_node=$N_PROC_PER_NODE \
-      --nnodes=$N_NODES \
-      --node_rank $NODE_RANK \
-      --master_addr $MASTER_ADDR \
-      --master_port $MASTER_PORT \
-    ./train_mlm_camembert_thai.ddp.py \
-      --tokenizer_name_or_path ../dataset/spm/thwiki-for-ddp_6.11.2020_spm_vs-24k/sentencepiece.bpe.model \
-      --ext txt \
-      --train_path ../dataset/split/thwiki-for-ddp_6.11.2020/train/train.txt \
-      --eval_path ../dataset/split/thwiki-for-ddp_6.11.2020/val/val.txt \
-      --block_size 510 \
-      --learning_rate $LR --weight_decay 0.01 \
-      --adam_epsilon 1e-6 \
-      --fp16 True \
-      --max_steps $MAX_STEPS \
-      --per_device_train_batch_size $BATCH_SIZE \
-      --per_device_eval_batch_size $BATCH_SIZE \
-      --gradient_accumulation_steps $GRAD_ACC \
-      --warmup_steps $WARMUP_STEPS \
-      --seed 2020 \
-      --save_steps $SAVE_STEPS \
-      --logging_steps 10 \
-      --save_total_limit 100 \
-      --evaluation_strategy steps \
-      --eval_steps $EVAL_STEPS \
-      --logging_dir /ist/ist-share/scads/aires/thai2transformers_store/logs/$EXP_NAME/ \
-      --output_dir /ist/ist-share/scads/aires/thai2transformers_store/checkpoints/$EXP_NAME/ \
-      --add_space_token \
-      --datasets_cache_dir ../dataset/binarized/thwiki-for-ddp_6.11.2020/ \
-      --dataset_loader_name linebyline |& tee -a ./slurm_logs/$EXP_NAME.job-$JOBID.rank-$NODE_RANK.out
-fi
+
+python -m torch.distributed.launch \
+  --nproc_per_node=$N_PROC_PER_NODE \
+  --nnodes=$N_NODES \
+  --node_rank $NODE_RANK \
+  --master_addr $MASTER_ADDR \
+  --master_port $MASTER_PORT \
+  ./train_mlm_camembert_thai.ddp.py \
+  --tokenizer_name_or_path ../dataset/spm/thwiki-for-ddp_6.11.2020_spm_vs-24k/sentencepiece.bpe.model \
+  --ext txt \
+  --train_path ../dataset/split/thwiki-for-ddp_6.11.2020/train/train.txt \
+  --eval_path ../dataset/split/thwiki-for-ddp_6.11.2020/val/val.txt \
+  --block_size 510 \
+  --learning_rate $LR --weight_decay 0.01 \
+  --adam_epsilon 1e-6 \
+  --fp16 True \
+  --max_steps $MAX_STEPS \
+  --per_device_train_batch_size $BATCH_SIZE \
+  --per_device_eval_batch_size $BATCH_SIZE \
+  --gradient_accumulation_steps $GRAD_ACC \
+  --warmup_steps $WARMUP_STEPS \
+  --seed 2020 \
+  --save_steps $SAVE_STEPS \
+  --logging_steps 10 \
+  --save_total_limit 100 \
+  --evaluation_strategy steps \
+  --eval_steps $EVAL_STEPS \
+  --logging_dir /ist/ist-share/scads/aires/thai2transformers_store/logs/$EXP_NAME/ \
+  --output_dir /ist/ist-share/scads/aires/thai2transformers_store/checkpoints/$EXP_NAME/ \
+  --add_space_token \
+  --datasets_cache_dir ../dataset/binarized/thwiki-for-ddp_6.11.2020/ \
+  --dataset_loader_name linebyline |& tee -a ./slurm_logs/$EXP_NAME.job-$JOBID.rank-$NODE_RANK.out
