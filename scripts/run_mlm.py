9c1a0e4f25cc17cff17b273ae26634d56b572a1a
Zinc Orca <zinc.orca@gmail.com>
2020-11-16 09:55:12 +0700

    Fix remove debug code from run_mlm.py

--- scripts/run_mlm.py
+++ scripts/run_mlm.py
@@ -329,7 +329,4 @@ def _mp_fn(index):
 
 
 if __name__ == "__main__":
-    print(sys.argv)
-    del sys.argv[1:]
-    sys.argv.extend(['--tokenizer_name_or_path', '../data/input/zo_test/sentencepiece.bpe.model', '--ext', 'txt', '--train_dir', '../data/input/zo_test/text/train', '--eval_dir', '../data/input/zo_test/text/train', '--max_seq_length', '416', '--learning_rate', '3e-4', '--weight_decay', '0.01', '--adam_epsilon', '1e-6', '--max_steps', '100', '--per_device_train_batch_size', '1', '--per_device_eval_batch_size', '1', '--gradient_accumulation_steps', '1', '--warmup_steps', '24000', '--seed', '2020', '--save_steps', '100', '--logging_steps', '5', '--save_total_limit', '50', '--evaluate_during_training', '--eval_steps', '2500', '--logging_dir', '../data/output/zo_test/logs', '--output_dir', '../data/output/zo_test/model', '--add_space_token', '--model_dir', '../data/input/zo_test/model/model_only', '--datasets_cache_dir', '../cache', '--preprocessing_num_workers', '1', '--datasets_map_batch_size', '1000', '--writer_batch_size', '1000'])
     main()
