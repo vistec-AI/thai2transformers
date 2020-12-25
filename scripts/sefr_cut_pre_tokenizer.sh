b19a79b7a30391064564c910f6bd3e77966111bc
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2020-12-25 10:50:09 +0700

    pre-dev: Remove internal and unused files

--- scripts/sefr_cut_pre_tokenizer.sh
+++
@@ -1,9 +0,0 @@
-arguements=$(cat <<-EOF
- --input_folder ../data/input/datasets/thwiki-for-ddp_concat_12.11.2020/val
- --output_folder ../data/input/datasets/thwiki-for-ddp_concat_12.11.2020_pre_tokenized/val
- --chunk_size $(($(nproc) * 200))
- --overwrite
-EOF
-)
-
-python3 sefr_cut_pre_tokenizer.py $arguements
