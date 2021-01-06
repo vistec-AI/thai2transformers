101ed1863a496fcfa54f6e051cf97bbd184d1b6f
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-01-06 13:00:35 +0700

    Add metrics for multi-label classification

--- thai2transformers/metrics.py
+++ thai2transformers/metrics.py
@@ -17,3 +17,21 @@ def classification_metrics(pred, pred_labs=False):
         'recall_macro': recall_macro,
         'nb_samples': len(labels)
     }
+
+def multilabel_classification_metrics(pred, pred_labs=False):
+
+    labels = pred.label_ids
+    preds = np.array(pred.predictions >= 0.5)
+    precision_macro, recall_macro, f1_macro, _ = precision_recall_fscore_support(labels, preds, average='macro')
+    precision_micro, recall_micro, f1_micro, _ = precision_recall_fscore_support(labels, preds, average='micro')
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+        'f1_micro': f1_micro,
+        'precision_micro': precision_micro,
+        'recall_micro': recall_micro,
+        'f1_macro': f1_macro,
+        'precision_macro': precision_macro,
+        'recall_macro': recall_macro,
+        'nb_samples': len(labels)
+    }
