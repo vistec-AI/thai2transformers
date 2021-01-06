533e14bbffd15b24b697b4581ab2d4863a952618
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-01-06 12:49:16 +0700

    Fix error, TypeError: 'Tensor' object is not callable

--- thai2transformers/models.py
+++ thai2transformers/models.py
@@ -75,7 +75,7 @@ class RobertaForMultiLabelSequenceClassification(RobertaPreTrainedModel):
         if labels is not None:
             if self.num_labels > 1:
                 loss_fct = torch.nn.BCEWithLogitsLoss()
-                loss = loss_fct(logits(self.num_labels), labels)
+                loss = loss_fct(logits, labels)
 
         if not return_dict:
             output = (logits,) + outputs[2:]
