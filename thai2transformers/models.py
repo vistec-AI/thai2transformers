9a4492fd37fc13734b093c0ed1dfa3136a22fdaa
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-01-06 11:09:05 +0700

    Add RobertaForMultiLabelSequenceClassification class

---
+++ thai2transformers/models.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+
+from thai2transformers.metrics import classification_metrics
+from thai2transformers.datasets import (
+    SequenceClassificationDataset,
+    TokenClassificationDataset
+)
+from transformers import (
+    AdamW,
+    get_linear_schedule_with_warmup,
+    AutoTokenizer,
+    AutoModel
+)
+from transformers.modeling_outputs import (
+    SequenceClassifierOutput
+)
+from transformers.modeling_roberta import (
+    RobertaPreTrainedModel,
+    RobertaModel,
+    RobertaClassificationHead
+)
+
+class RobertaForMultiLabelSequenceClassification(RobertaPreTrainedModel):
+    authorized_missing_keys = [r"position_ids"]
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
+        self.classifier = RobertaClassificationHead(config)
+
+        self.init_weights()  
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for computing the sequence classification/regression loss.
+            Indices should be in :obj:`[0, ..., config.num_labels - 1]`.
+            If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
+            If :obj:`config.num_labels > 1` a multilabel classification loss is computed (BCELoss).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        sequence_output = outputs[0]
+        logits = self.classifier(sequence_output)
+
+        loss = None
+        if labels is not None:
+            if self.num_labels > 1:
+                loss_fct = torch.nn.BCEWithLogitsLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
