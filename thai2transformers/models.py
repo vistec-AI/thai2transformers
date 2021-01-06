c69575da4b90e8d1c17e5f735b30fd66544fe465
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-01-06 14:13:32 +0700

    Add Bert, and XLMRoberta model subclass for multi-label classification tasks

--- thai2transformers/models.py
+++ thai2transformers/models.py
@@ -17,11 +17,88 @@ from transformers import (
 from transformers.modeling_outputs import (
     SequenceClassifierOutput
 )
+
+from transformers.modeling_bert import (
+    BertPreTrainedModel,
+    BertModel,
+    BertClassificationHead
+)
+
 from transformers.modeling_roberta import (
     RobertaPreTrainedModel,
     RobertaModel,
     RobertaClassificationHead
 )
+
+from transformers.modeling_xlmroberta import (
+    XLMRobertaConfig
+)
+
+class BertMultiLabelForSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = BertModel(config)
+        self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = torch.nn.Linear(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for computing the sequence classification/regression loss.
+            Indices should be in :obj:`[0, ..., config.num_labels - 1]`.
+            If :obj:`config.num_labels > 1` a multilabel classification loss is computed (BCELoss).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            loss_fct = torch.nn.BCEWithLogitsLoss()
+            loss = loss_fct(logits, labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
 
 class RobertaForMultiLabelSequenceClassification(RobertaPreTrainedModel):
     authorized_missing_keys = [r"position_ids"]
@@ -52,7 +129,6 @@ class RobertaForMultiLabelSequenceClassification(RobertaPreTrainedModel):
         labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
             Labels for computing the sequence classification/regression loss.
             Indices should be in :obj:`[0, ..., config.num_labels - 1]`.
-            If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
             If :obj:`config.num_labels > 1` a multilabel classification loss is computed (BCELoss).
         """
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
@@ -87,3 +163,11 @@ class RobertaForMultiLabelSequenceClassification(RobertaPreTrainedModel):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
+
+class XLMRobertaForMultiLabelSequenceClassification(RobertaForMultiLabelSequenceClassification):
+    """
+    This class overrides :class:`~thai2transformers.RobertaForMultiLabelSequenceClassificationn`. Please check the
+    superclass for the appropriate documentation alongside usage examples.
+    """
+
+    config_class = XLMRobertaConfig
