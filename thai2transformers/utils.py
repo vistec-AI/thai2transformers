b259c92154ad54d282145fdac4ab8e1101f4a9db
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-01-08 19:53:42 +0700

    Change logic of get_dict_val

--- thai2transformers/utils.py
+++ thai2transformers/utils.py
@@ -1,36 +1,33 @@
 import operator
 from functools import reduce
-from typing import List, Dict, Union
+from typing import Tuple, Dict, Union
 
 
-def get_dict_val(root:Dict, keys:Union[str, List[str]]):
+def get_dict_val(root:Dict, keys:Union[str, Tuple[str, str]]):
     """
     Access a nested object in root by item sequence.
 
     Args:
         root: Dict
             target object for accessing the value
-        keys: Union[str, List[str]]
+        keys: Union[str, Tuple[str, str]]
             a key or a list of key (for nested structure objecy) name
             to traverse through the Dict object 
 
     Examples::
 
-        >>> obj = {"a": {"b": 100, "c": 0}, "d": 1}
-        >>> get_dict_val(obj, "d")
-        1
-
-        >>> obj = {"a": {"b": 100, "c": 0}, "d": 1}
+        >>> obj = {"a": [1,2,3]}
         >>> get_dict_val(obj, "a")
-        {"b": 100, "c": 0}
+        [1,2,3]
 
-        >>> obj = {"a": {"b": 100, "c": 0}, "d": 1}
-        >>> get_dict_val(obj, ["a","b"])
-        100
+        >>> obj = {"a": [ {"aa": 100, "bb": 0}, {"aa": 2, "bb": 5 } ] }
+        >>> get_dict_val(obj, ("a", "aa"))
+        [100, 2]
+
     """
     if type(keys) == str:
         return root[keys]
-    elif type(keys) == list:
-        return reduce(operator.getitem, keys, root)
+    elif type(keys) == tuple:
+        return list(map(lambda x: x[keys[1]], root[keys[0]]))
     
     return None
