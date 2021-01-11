426239db862bff89bc328cf3b26e005e79ba66ac
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-01-11 11:16:13 +0700

    Fix an issue where datasets.map can't access text from a nested dictionary

--- thai2transformers/utils.py
+++ thai2transformers/utils.py
@@ -27,7 +27,7 @@ def get_dict_val(root:Dict, keys:Union[str, Tuple[str, str]]):
     """
     if type(keys) == str:
         return root[keys]
-    elif type(keys) == tuple:
-        return list(map(lambda x: x[keys[1]], root[keys[0]]))
+    elif type(keys) == tuple and len(keys) == 2:
+        return root[keys[0]][keys[1]]
     
     return None
