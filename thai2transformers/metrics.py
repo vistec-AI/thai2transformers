c1fe932ad7610f970e45828e2e548b102ab5a860
Charin <charin.polpanumas@datatouille.org>
2021-01-12 17:18:33 +0700

    add `seqeval_classification_metrics`
    to measure NER tasks

--- thai2transformers/metrics.py
+++ thai2transformers/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from seqeval.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 
 def classification_metrics(pred, pred_labs=False):
     labels = pred.label_ids
@@ -16,4 +17,26 @@ def classification_metrics(pred, pred_labs=False):
         'precision_macro': precision_macro,
         'recall_macro': recall_macro,
         'nb_samples': len(labels)
+    }
+
+def seqeval_classification_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions
+    precision_macro = precision_score(labels, preds, average='macro')
+    recall_macro = recall_score(labels, preds, average='macro')
+    f1_macro = f1_score(labels, preds, average='macro')
+    precision_micro = precision_score(labels, preds, average='micro')
+    recall_micro = recall_score(labels, preds, average='micro')
+    f1_micro = f1_score(labels, preds, average='micro')
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+        'f1_micro': f1_micro,
+        'precision_micro': precision_micro,
+        'recall_micro': recall_micro,
+        'f1_macro': f1_macro,
+        'precision_macro': precision_macro,
+        'recall_macro': recall_macro,
+        'nb_samples': len(labels),
+        'classification_report': classification_report(labels, preds, digits=4)
     }
