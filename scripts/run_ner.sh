a3ebc6034db6359e6486c34a5f1208fbeb44af89
Zinc Orca <zinc.orca@gmail.com>
2021-01-13 08:49:45 +0700

    Show example of how to caculate warmup_steps in run_ner.sh.

--- scripts/run_ner.sh
+++ scripts/run_ner.sh
@@ -1,15 +1,15 @@
 cat $0
-EXP_NAME=thainer-syllable-3epoch
-python3 run_ner.py --tokenizer_type ThaiWordsSyllableTokenizer \
- --tokenizer_name_or_path /ist/ist-share/scads/zo/thai2transformers/exp/syllable/tokenizer_folder \
- --model_name_or_path /ist/ist-share/scads/zo/thai2transformers/exp/syllable/output_folder/model/checkpoint-8000 \
+EXP_NAME=thainer-xlm-3epoch
+python3 run_ner.py --tokenizer_type AutoTokenizer \
+ --tokenizer_name_or_path xlm-roberta-base \
+ --model_name_or_path xlm-roberta-base \
  --dataset_name thainer \
  --label_name ner_tags \
  --per_device_train_batch_size 16 \
  --per_device_eval_batch_size 16 \
  --gradient_accumulation_steps 1 \
  --learning_rate 5e-5 \
- --warmup_steps 1000 \
+ --warmup_steps $((159 * 3 / 10))  \
  --logging_steps 10 \
  --eval_steps 159 \
  --max_steps $((159 * 3)) \
