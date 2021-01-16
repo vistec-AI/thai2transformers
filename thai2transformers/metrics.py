c1adb3d7184d2731a649d1e1cdb8cdb5057f827d
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-01-16 13:09:15 +0700

    Disable seqeval

--- thai2transformers/metrics.py
+++ thai2transformers/metrics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support, f1_score
-from seqeval.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+# from seqeval.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 
 def classification_metrics(pred, pred_labs=False):
     labels = pred.label_ids
@@ -28,7 +28,7 @@ def _compute_best_threshold(targets, probs):
             threshold/100,
             f1_score(targets,
                      preds,
-                     average='macro')
+                     average='binary')
         ))
 
     f1s_df = pd.DataFrame(f1s).sort_values(1,ascending=False).reset_index(drop=True)
@@ -74,24 +74,24 @@ def multilabel_classification_metrics(pred, n_labels):
         'nb_samples': len(labels)
     }
 
-def seqeval_classification_metrics(pred):
-    labels = pred.label_ids
-    preds = pred.predictions
-    precision_macro = precision_score(labels, preds, average='macro')
-    recall_macro = recall_score(labels, preds, average='macro')
-    f1_macro = f1_score(labels, preds, average='macro')
-    precision_micro = precision_score(labels, preds, average='micro')
-    recall_micro = recall_score(labels, preds, average='micro')
-    f1_micro = f1_score(labels, preds, average='micro')
-    acc = accuracy_score(labels, preds)
-    return {
-        'accuracy': acc,
-        'f1_micro': f1_micro,
-        'precision_micro': precision_micro,
-        'recall_micro': recall_micro,
-        'f1_macro': f1_macro,
-        'precision_macro': precision_macro,
-        'recall_macro': recall_macro,
-        'nb_samples': len(labels),
-        'classification_report': classification_report(labels, preds, digits=4)
-    }
+# def seqeval_classification_metrics(pred):
+#     labels = pred.label_ids
+#     preds = pred.predictions
+#     precision_macro = precision_score(labels, preds, average='macro')
+#     recall_macro = recall_score(labels, preds, average='macro')
+#     f1_macro = f1_score(labels, preds, average='macro')
+#     precision_micro = precision_score(labels, preds, average='micro')
+#     recall_micro = recall_score(labels, preds, average='micro')
+#     f1_micro = f1_score(labels, preds, average='micro')
+#     acc = accuracy_score(labels, preds)
+#     return {
+#         'accuracy': acc,
+#         'f1_micro': f1_micro,
+#         'precision_micro': precision_micro,
+#         'recall_micro': recall_micro,
+#         'f1_macro': f1_macro,
+#         'precision_macro': precision_macro,
+#         'recall_macro': recall_macro,
+#         'nb_samples': len(labels),
+#         'classification_report': classification_report(labels, preds, digits=4)
+#     }
