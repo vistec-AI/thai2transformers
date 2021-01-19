8db4e28b33fb8b2517919d9553dacec4512ee91a
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-01-19 08:45:39 +0700

    Remove redundant code

--- thai2transformers/metrics.py
+++ thai2transformers/metrics.py
@@ -21,27 +21,6 @@ def sk_classification_metrics(pred, pred_labs=False):
         'classification_report': classification_report(labels, preds, digits=4)
     }
 
-# from sklearn.metrics import accuracy_score, precision_recall_fscore_support, f1_score
-from seqeval.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
-
-def classification_metrics(pred, pred_labs=False):
-    labels = pred.label_ids
-    preds = pred.predictions if pred_labs else pred.predictions.argmax(-1) 
-    precision_macro, recall_macro, f1_macro, _ = precision_recall_fscore_support(labels, preds, average='macro')
-    precision_micro, recall_micro, f1_micro, _ = precision_recall_fscore_support(labels, preds, average='micro')
-    acc = accuracy_score(labels, preds)
-    return {
-        'accuracy': acc,
-        'f1_micro': f1_micro,
-        'precision_micro': precision_micro,
-        'recall_micro': recall_micro,
-        'f1_macro': f1_macro,
-        'precision_macro': precision_macro,
-        'recall_macro': recall_macro,
-        'nb_samples': len(labels)
-    }
-
-
 def seqeval_classification_metrics(pred):
     from seqeval.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
     labels = pred.label_ids
@@ -118,25 +97,3 @@ def multilabel_classification_metrics(pred, n_labels):
         'recall_macro': recall_macro,
         'nb_samples': len(labels)
     }
-
-# def seqeval_classification_metrics(pred):
-#     labels = pred.label_ids
-#     preds = pred.predictions
-#     precision_macro = precision_score(labels, preds, average='macro')
-#     recall_macro = recall_score(labels, preds, average='macro')
-#     f1_macro = f1_score(labels, preds, average='macro')
-#     precision_micro = precision_score(labels, preds, average='micro')
-#     recall_micro = recall_score(labels, preds, average='micro')
-#     f1_micro = f1_score(labels, preds, average='micro')
-#     acc = accuracy_score(labels, preds)
-#     return {
-#         'accuracy': acc,
-#         'f1_micro': f1_micro,
-#         'precision_micro': precision_micro,
-#         'recall_micro': recall_micro,
-#         'f1_macro': f1_macro,
-#         'precision_macro': precision_macro,
-#         'recall_macro': recall_macro,
-#         'nb_samples': len(labels),
-#         'classification_report': classification_report(labels, preds, digits=4)
-#     }
