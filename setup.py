29b93635ebbe2cab574ccd88b103e5e46754587e
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-01-29 17:47:20 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+  readme =  f.read()
+
+requirements = [
+    "torch>=1.0",
+    "pythainlp>=2.1.0",
+    "emoji",
+    "transformers==3.5.0",
+    "dataset>=1.2.1",
+]
+
+
+setup(
+    name="thai2transformers",
+    version="0.0.1dev0",
+    description="Pretraining transformer based Thai language models ",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="VISTEC-depa AI Research Institute of Thailand",
+    author_email="",
+    url="https://github.com/vistec-AI/thai2transformers",
+    packages=find_packages(exclude=["notebooks", "notebooks.*", "external_scripts", "external_scripts.*]),
+    test_suite="tests",
+    python_requires=">=3.6",
+    package_data={},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords=[
+        "thainlp",
+        "NLP",
+        "natural language processing",
+        "text analytics",
+        "text processing",
+        "localization",
+        "computational linguistics",
+        "ThaiNLP",
+        "Thai NLP",
+        "Thai language",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: Thai",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    project_urls={
+        "Documentation": "https://github.com/vistec-AI/thai2transformers",
+        "Tutorials": "https://colab.research.google.com/drive/1Kbk6sBspZLwcnOE61adAQo30xxqOQ9ko?usp=sharing",
+        "Source Code": "https://github.com/vistec-AI/thai2transformers",
+        "Bug Tracker": "https://github.com/vistec-AI/thai2transformers/issues",
+    },
+)
