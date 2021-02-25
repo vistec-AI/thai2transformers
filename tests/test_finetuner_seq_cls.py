d6dd90d7850a1fe8b034ce5d93a34a0ff3564a5b
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-02-25 21:26:49 +0700

    Add @required_torch annotation and test case for BaseFinetuner

--- tests/test_finetuner_seq_cls.py
+++ tests/test_finetuner_seq_cls.py
@@ -1,6 +1,9 @@
 import unittest
 
+from transformers.testing_utils import require_torch
+
 from thai2transformers.finetuner import (
+    BaseFinetuner,
     SequenceClassificationFinetuner,
 )
 
@@ -11,6 +14,7 @@ from thai2transformers.tokenizers import (
     FakeSefrCutTokenizer
 )
 
+from thai2transformers.conf import Task
 
 from transformers import (
     CamembertTokenizer,
@@ -34,7 +38,15 @@ class TestSequenceClassificationFinetuner(unittest.TestCase):
     def test_init(self):
         seq_cls_finetuner = SequenceClassificationFinetuner()
         self.assertIsNotNone(seq_cls_finetuner)
+    
+    def test_base_finetuner(self):
+        base_finetuner = BaseFinetuner()
+        self.assertIsNotNone(base_finetuner)
+        base_finetuner.load_pretrained_tokenizer()
+        base_finetuner.load_pretrained_model()
+        base_finetuner.finetune()
 
+    @require_torch
     def test_load_pretrained_tokenizer_wangchanberta_spm_camembert(self):
         
         pretrained_tokenizer_name = 'airesearch/wangchanberta-base-att-spm-uncased'
@@ -50,6 +62,7 @@ class TestSequenceClassificationFinetuner(unittest.TestCase):
         self.assertEqual(seq_cls_finetuner.tokenizer.additional_special_tokens,
                          ['<s>NOTUSED', '</s>NOTUSED', '<_>'])
 
+    @require_torch
     def test_load_pretrained_tokenizer_wangchanberta_spm(self):
         
         pretrained_tokenizer_name = 'airesearch/wangchanberta-base-wiki-spm'
@@ -62,7 +75,8 @@ class TestSequenceClassificationFinetuner(unittest.TestCase):
         )
 
         self.assertEqual(seq_cls_finetuner.tokenizer.__class__.__name__,'ThaiRobertaTokenizer')
-
+    
+    @require_torch
     def test_load_pretrained_tokenizer_wangchanberta_newmm(self):
         
         pretrained_tokenizer_name = 'airesearch/wangchanberta-base-wiki-newmm'
@@ -76,6 +90,7 @@ class TestSequenceClassificationFinetuner(unittest.TestCase):
 
         self.assertEqual(seq_cls_finetuner.tokenizer.__class__.__name__,'ThaiWordsNewmmTokenizer')
     
+    @require_torch
     def test_load_pretrained_tokenizer_wangchanberta_syllable(self):
         
         pretrained_tokenizer_name = 'airesearch/wangchanberta-base-wiki-ssg'
@@ -89,6 +104,7 @@ class TestSequenceClassificationFinetuner(unittest.TestCase):
 
         self.assertEqual(seq_cls_finetuner.tokenizer.__class__.__name__,'ThaiWordsSyllableTokenizer')
 
+    @require_torch
     def test_load_pretrained_tokenizer_wangchanberta_sefr(self):
         
         pretrained_tokenizer_name = 'airesearch/wangchanberta-base-wiki-sefr'
@@ -102,6 +118,7 @@ class TestSequenceClassificationFinetuner(unittest.TestCase):
 
         self.assertEqual(seq_cls_finetuner.tokenizer.__class__.__name__,'FakeSefrCutTokenizer')
     
+    @require_torch
     def test_load_pretrained_tokenizer_xlmr(self):
         
         pretrained_tokenizer_name = 'xlm-roberta-base'
@@ -114,7 +131,8 @@ class TestSequenceClassificationFinetuner(unittest.TestCase):
         )
 
         self.assertEqual(seq_cls_finetuner.tokenizer.__class__.__name__,'XLMRobertaTokenizer')
-      
+    
+    @require_torch
     def test_load_pretrained_tokenizer_mbert(self):
         
         pretrained_tokenizer_name = 'bert-base-multilingual-cased'
@@ -127,7 +145,85 @@ class TestSequenceClassificationFinetuner(unittest.TestCase):
         )
 
         self.assertEqual(seq_cls_finetuner.tokenizer.__class__.__name__,'BertTokenizer')
-      
+    
+    @require_torch
+    def test_load_pretrained_model_for_seq_cls_incorrect_task(self):
+        pretrained_model_name = 'airesearch/wangchanberta-base-att-spm-uncased'
+        
+        # instantiate RobertaModelForSequenceClassification without `num_labels`
+        seq_cls_finetuner = SequenceClassificationFinetuner()
+        task = 'token_classification'
+        with self.assertRaises(NotImplementedError) as context:
+            seq_cls_finetuner.load_pretrained_model(
+                task=task,
+                name_or_path=pretrained_model_name
+            )
+
+        self.assertEqual(
+            f'The task specified `{task}` is incorrect or not available for SequenceClassificationFinetuner',
+            str(context.exception))
+
+    @require_torch
+    def test_load_pretrained_model_for_seq_cls_wangchanberta_spm_camembert(self):
+        
+        pretrained_model_name = 'airesearch/wangchanberta-base-att-spm-uncased'
+        
+        # instantiate RobertaForSequenceClassification without `num_labels`
+        seq_cls_finetuner = SequenceClassificationFinetuner()
+        seq_cls_finetuner.load_pretrained_model(
+            task='multiclass_classification',
+            name_or_path=pretrained_model_name
+
+        )
+        self.assertEqual(seq_cls_finetuner.model.__class__.__name__,'RobertaForSequenceClassification')
+        self.assertEqual(seq_cls_finetuner.config.num_labels, 2) # num_labels = 2 is the default value
+        
+        # instantiate RobertaForSequenceClassification with `num_labels` specified
+        seq_cls_finetuner = SequenceClassificationFinetuner()
+        seq_cls_finetuner.load_pretrained_model(
+            task='multiclass_classification',
+            name_or_path=pretrained_model_name,
+            num_labels=10
+        )
+        self.assertEqual(seq_cls_finetuner.model.__class__.__name__,'RobertaForSequenceClassification')
+        self.assertEqual(seq_cls_finetuner.config.num_labels, 10)
+
+        # instantiate RobertaForSequenceClassification with `num_labels` specified and using Task enum variable
+        seq_cls_finetuner = SequenceClassificationFinetuner()
+        seq_cls_finetuner.load_pretrained_model(
+            task=Task.MULTICLASS_CLS,
+            name_or_path=pretrained_model_name,
+            num_labels=10
+        )
+        self.assertEqual(seq_cls_finetuner.model.__class__.__name__,'RobertaForSequenceClassification')
+        self.assertEqual(seq_cls_finetuner.config.num_labels, 10)
+
+        # instantiate RobertaForMultiLabelSequenceClassification without `num_labels`
+        seq_cls_finetuner = SequenceClassificationFinetuner()
+        seq_cls_finetuner.load_pretrained_model(
+            task='multilabel_classification',
+            name_or_path=pretrained_model_name
+        )
+        self.assertEqual(seq_cls_finetuner.model.__class__.__name__,'RobertaForMultiLabelSequenceClassification')
+        self.assertEqual(seq_cls_finetuner.config.num_labels, 2) # num_labels = 2 is the default value
+        
+        # instantiate RobertaForMultiLabelSequenceClassification with `num_labels` specified
+        seq_cls_finetuner = SequenceClassificationFinetuner()
+        seq_cls_finetuner.load_pretrained_model(
+            task='multilabel_classification',
+            name_or_path=pretrained_model_name,
+            num_labels=10
+        )
+        self.assertEqual(seq_cls_finetuner.model.__class__.__name__,'RobertaForMultiLabelSequenceClassification')
+        self.assertEqual(seq_cls_finetuner.config.num_labels, 10)
 
 
-    # def test_load_pretrained_model(self):
+        # instantiate RobertaForMultiLabelSequenceClassification with `num_labels` specified and using Task enum variable
+        seq_cls_finetuner = SequenceClassificationFinetuner()
+        seq_cls_finetuner.load_pretrained_model(
+            task=Task.MULTILABEL_CLS,
+            name_or_path=pretrained_model_name,
+            num_labels=10
+        )
+        self.assertEqual(seq_cls_finetuner.model.__class__.__name__,'RobertaForMultiLabelSequenceClassification')
+        self.assertEqual(seq_cls_finetuner.config.num_labels, 10)
