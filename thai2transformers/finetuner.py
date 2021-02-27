718f922076c943157ae3024e96a6ee03717a64d4
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-02-27 14:31:52 +0700

    Add method finetune

--- thai2transformers/finetuner.py
+++ thai2transformers/finetuner.py
@@ -4,15 +4,25 @@ from typing import List, Dict, Union, Optional, Callable
 from transformers import (
     AutoConfig,
     AutoModelForSequenceClassification,
+    TrainingArguments,
+    Trainer,
 )
 from transformers.tokenization_utils import (
     PreTrainedTokenizer,
 )
 from .auto import (
-    AutoModelForMultiLabelSequenceClassification
+    AutoModelForMultiLabelSequenceClassification,
 )
 from .conf import (
-    Task
+    Task,
+)
+from .datasets import (
+    SequenceClassificationDataset,
+    TokenClassificationDataset,
+)
+from .metrics import (
+    classification_metrics,
+    multilabel_classification_metrics,
 )
 
 AIRESEARCH_MODEL_PREFIX = 'airesearch/wangchanberta'
@@ -20,6 +30,11 @@ AIRESEARCH_MODEL_NAME = {
     f'{AIRESEARCH_MODEL_PREFIX}-base-att-spm-uncased': {
         'space_token': '<_>'
     }
+}
+
+FINETUNE_SEQ_CLS_METRIC_MAPPING = {
+    Task.MULTICLASS_CLS.value: classification_metrics,
+    Task.MULTILABEL_CLS.value: multilabel_classification_metrics
 }
 
 FINETUNE_SEQ_CLS_MODEL_MAPPING = {
@@ -45,12 +60,15 @@ class SequenceClassificationFinetuner:
                 config = None,
                 task: Union[str, Task] = None,
                 num_labels: int = None,
-                *kwargs):
+                metric: Union[str, Callable, Task] = None):
 
         self.tokenizer = tokenizer
         self.config = config
         self.task = task.value if type(task) == Task else task
         self.num_labels = num_labels
+        self.metric = metric
+        self.training_args = None
+        self.trainer = None
 
     def load_pretrained_tokenizer(self,
             tokenizer_cls: PreTrainedTokenizer,
@@ -71,14 +89,13 @@ class SequenceClassificationFinetuner:
                     AIRESEARCH_MODEL_NAME[name_or_path]['space_token']
                 ]
 
-
     def load_pretrained_model(self, 
-        task: Union[str, Task],
-        name_or_path: Union[str, os.PathLike],
-        num_labels: int = None):
+                              task: Union[str, Task],
+                              name_or_path: Union[str, os.PathLike],
+                              num_labels: int = None):
         """
         Load a pretrained model to the finetuner instance and modify classification head
-        according to the specified `task` in the method argument.
+        and metric according to the specified `task` in the method argument.
 
         Arguments:
             task: Union[str, Task],
@@ -101,3 +118,45 @@ class SequenceClassificationFinetuner:
 
         self.model = FINETUNE_SEQ_CLS_MODEL_MAPPING[task].from_pretrained(name_or_path,
                                                         config=self.config)
+        self.metric = FINETUNE_SEQ_CLS_METRIC_MAPPING[task]
+
+    def _init_trainer(self,
+                      training_args,
+                      train_dataset: SequenceClassificationDataset,
+                      val_dataset: SequenceClassificationDataset = None):
+        
+        self.training_args = TrainingArguments(training_args)
+        data_collator = DataCollatorWithPadding(tokenizer,
+                                                padding=True,
+                                                pad_to_multiple_of=8 if kwargs['fp16'] else None)
+
+        self.trainer = Trainer(
+            model=self.model,
+            args=self.training_args,
+            compute_metrics=self.metric,
+            train_dataset=train_dataset,
+            eval_dataset=val_dataset,
+            data_collator=data_collator
+        )
+
+    def finetune(self,
+                 training_args,
+                 train_dataset: SequenceClassificationDataset,
+                 val_dataset: SequenceClassificationDataset = None,
+                 test_dataset: SequenceClassificationDataset = None):
+
+        self._init_trainer()
+        self.trainer.train()
+        self.trainer.save_model(
+            os.path.join(training_args.output_dir,
+            'checkpoint-final')
+        )
+
+        if test_dataset != None:
+
+        _, label_ids, result = trainer.predict(test_dataset=test_dataset)
+    
+        print(f'Evaluation on test set')    
+    
+        for key, value in result.items():
+            print(f'{key} : {value:.4f}')
