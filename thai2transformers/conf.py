e4beb26be3e43fe1a9f207e28804c78caf575cd4
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-03-01 10:33:26 +0700

    Add tasks for token-level and chunk-level classification

--- thai2transformers/conf.py
+++ thai2transformers/conf.py
@@ -4,3 +4,5 @@ from enum import Enum
 class Task(Enum):
     MULTICLASS_CLS = 'multiclass_classification'
     MULTILABEL_CLS = 'multilabel_classification'
+    CHUNK_LEVEL_CLS = 'chunk_level_classification'
+    TOKEN_LEVEL_CLS = 'token_level_classification'
