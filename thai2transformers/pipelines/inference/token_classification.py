a82b1bfe42c52b5376fd909cb82832a31fc8f59b
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-03-25 10:37:31 +0700

    Replace special symbol `space_token` to space " "

--- thai2transformers/pipelines/inference/token_classification.py
+++ thai2transformers/pipelines/inference/token_classification.py
@@ -230,7 +230,8 @@ class TokenClassificationPipeline:
             text = ''
             ne = ne_position_mapping[0][1]
             for ne_position in ne_position_mapping:
-                text += tokens[ne_position[0]]
+                _token += tokens[ne_position[0]]
+                text += _token if _token != self.space_token else ' '
             groups.append({
                 'entity_group': ne,
                 'word': text
