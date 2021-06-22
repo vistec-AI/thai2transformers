2a83cc0b902bdeb7c50a24abf18edc5ded8fd3a4
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-06-22 13:27:44 +0700

    Add debugging message

--- scripts/train_mlm.py
+++ scripts/train_mlm.py
@@ -181,6 +181,8 @@ def main():
     logging.info(" Run name: %s", training_args.run_name)
     logging.info(" deepspeed: %s", training_args.deepspeed)
 
+    print('\nmodel:', model)
+    print('\ntraining_args:', training_args)
     # Initiate Trainer
     trainer = Trainer(
         model=model,
