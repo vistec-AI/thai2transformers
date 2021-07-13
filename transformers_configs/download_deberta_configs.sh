88083702e5e49f01504c8cc4dee4113d1d7dd582
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-07-13 11:21:10 +0700

    Rename directory

---
+++ transformers_configs/download_deberta_configs.sh
@@ -0,0 +1,3 @@
+curl https://huggingface.co/microsoft/deberta-base/resolve/main/config.json -O ./deberta-base-config.json
+curl https://huggingface.co/microsoft/deberta-large/resolve/main/config.json -O ./deberta-large-config.json
+curl https://huggingface.co/microsoft/deberta-xlarge/resolve/main/config.json -O ./deberta-xlarge-config.json
