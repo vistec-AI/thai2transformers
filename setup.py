3b10323364331a997385ae7f562142bd392b77d8
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-10-27 16:10:21 +0700

    Revert "Change transformers required version from 3.5.0 to 4.9.0"
    This reverts commit eb63efa905b4356acd96777724822a64da148df7.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 
 requirements = [
     "torch>=1.5.0",
-    "transformers==4.9.0",
+    "transformers==4.6.0dev0",
     "sentencepiece==0.1.95",
     "emoji",
     "pythainlp==2.2.4",
