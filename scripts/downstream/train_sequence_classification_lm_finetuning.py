5e8aaa7adc2d3490348a1aa5808c3c520a519ae2
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-11-11 16:37:10 +0700

    Define save_strategy for HF'Trainer as 'epoch'

--- scripts/downstream/train_sequence_classification_lm_finetuning.py
+++ scripts/downstream/train_sequence_classification_lm_finetuning.py
@@ -191,6 +191,7 @@ def init_trainer(task, model, train_dataset, val_dataset, warmup_steps, args, da
                         logging_first_step=False,
                         logging_steps=args.logging_steps,
                         #eval
+                        save_strategy='epoch',
                         evaluation_strategy='epoch' if 'validation' in DATASET_METADATA[args.dataset_name]['split_names'] else 'no',
                         load_best_model_at_end=True,
                         #others
